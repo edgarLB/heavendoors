d912677e740ff08e14769ddf4c05631fe999f568
edgarLB <103465521+edgarLB@users.noreply.github.com>
2024-09-08 20:06:56 -0700

    remove env

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,9 @@
 export default function About() {
-    const GOOGLE_KEY = import.meta.env.GOOGLE_MAPS_API_KEY;
-    const address = "24760+Sunnymead+Blvd+Ste+106,+Moreno+Valley,+CA+92553";
-    const url = `https://www.google.com/maps/embed/v1/place?key=${GOOGLE_KEY}&q=${address}`;
-    return (
+     return (
         <div className="page-container">
             <div className="page-body">
                 <h2>About</h2>
-                <iframe
-                    width="600"
-                    height="450"
-                    style={{border: 0}}
-                    loading="lazy"
-                    allowFullScreen
-                    referrerPolicy="no-referrer-when-downgrade"
-                    src={url}>
-                </iframe>
+
             </div>
         </div>
     )
