0c16d6dfc1615a8fcc1a9e336d2c4b9d39c5952d
edgarLB <103465521+edgarLB@users.noreply.github.com>
2024-09-09 15:18:30 -0700

    addef facebook link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
             </div>
             <div className="footer-item center">
                 <h4>Socials</h4>
-                <a className="facebook" href="" ><i className="fa-brands fa-facebook" style={{fontSize: '1.5em'}}></i></a>
+                <a className="facebook" href="https://www.facebook.com/people/Heaven-Doors-Horacio-Sanchez/pfbid02P3qTPRXRgid2NSAdqFNkYPjZveoipu4Rc5GRPVoC1FA1dzkbo2vXBoTkcmknBx1zl/?mibextid=pjQu9i" ><i className="fa-brands fa-facebook" style={{fontSize: '1.5em'}}></i></a>
             </div>
             <div className="footer-item last">
                 <h4>Find Us</h4>
@@ -16,7 +16,7 @@ export default function Footer() {
                     Moreno Valley, CA 92553</p>
                 <div className="hStack">
                     <i className="fa-solid fa-phone"></i>
-                    <p>(323) 550 - 0851</p>
+                    <p>(323) 540 - 0851</p>
                 </div>
             </div>
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ export default function Contact() {
                                 <p style={{fontWeight: 'bold'}}>Call Us: </p>
 
                             </div>
-                            <p style={{margin: '0', textAlign: "center"}}>(323) 550 - 0851</p>
+                            <p style={{margin: '0', textAlign: "center"}}>(323) 540 - 0851</p>
                         </div>
 
                         <div className="column-item">
@@ -24,7 +24,7 @@ export default function Contact() {
                             <div className="hStack">
 
                                 <i className="fa-solid fa-location-dot"></i>
-                                <p style={{fontWeight: 'bold'}}>Schedule a Vist:</p>
+                                <p style={{fontWeight: 'bold'}}>Schedule a Visit:</p>
                             </div>
                             <p style={{margin: '0', textAlign: "center"}}>2470 Sunnymead Blvd Ste 107<br/>
                                 Moreno Valley, CA 92553</p>
@@ -36,7 +36,7 @@ export default function Contact() {
                                 <p style={{fontWeight: 'bold'}}>Connect with Us:</p>
                             </div>
                             <div className="facebook-center">
-                                <a href="/home">
+                                <a href="https://www.facebook.com/people/Heaven-Doors-Horacio-Sanchez/">
                                     <div className="hStack facebook">
                                         <i className="fa-brands fa-square-facebook"></i>
                                         <p>Facebook</p>
