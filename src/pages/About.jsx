340aa7ff6e25cc66be2c3c11b4f3cf3d91867376
edgarLB <103465521+edgarLB@users.noreply.github.com>
2025-08-14 12:33:33 -0700

    About page google map added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,7 +21,16 @@ export default function About() {
                     <div className="column">
                         <h3>Visit Our Showroom</h3>
                         <p className="stock-banner">By Appointment Only</p>
-                        <img src={mapIMG} alt="map"/>
+                        <iframe
+                            title="Google Maps location"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3308.412099896791!2d-117.26213748478293!3d33.942756280635454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcaf5a5be4f14b%3A0x7e8c9ff553b8a3a!2s24760%20Sunnymead%20Blvd%20STE%20107%2C%20Moreno%20Valley%2C%20CA%2092553!5e0!3m2!1sen!2sus!4v1690000000000!5m2!1sen!2sus"
+                            width="100%"
+                            height="300"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        />
                         <p style={{margin: '0', textAlign: "center"}}>24760 Sunnymead Blvd Ste 107<br/>
                             Moreno Valley, CA 92553</p>
 
